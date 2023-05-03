f5edcef35736a9802fd1ed1751d804d03820f8d1
basantimages <124242939+basantimages@users.noreply.github.com>
2023-05-04 01:28:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,34 @@
+
 import cors from 'cors';
 import express from 'express';
-import axios from 'axios';
+import fetch from 'node-fetch';
+
 const app = express();
-
-// Section 1 =====================================================================
-const headers = {
-  'User-Agent':
-    'Instagram 105.0.0.18.119 (iPhone11,8; iOS 14_3; en_US; en-US; scale=2.00; 828x1792; 219842854)',
-  'x-ig-app-id': '1217981644879628',
-};
-
-// Add middleware to parse JSON request bodies
 app.use(express.json());
 app.use(cors());
 
-// Set CORS headers for all API endpoints
-app.use((_, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Referrer-Policy', 'no-referrer');
-  next();
-});
-
-app.post('/brs/data', async (req, res) => {
+app.post('/', async (req, res) => {
   try {
-    const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${req.body.input}`;
-
-    axios
-      .get(url, { headers })
-      .then((response) => {
-        console.log(response.data);
-        res.json(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    const response = await fetch(`https://www.instagram.com/${req.body.input}/?__a=1&__d=dis`);
+    const resp = await response.json();
+    console.log(resp);
+    res.json(resp);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch data' });
   }
 });
 
-app.get('/brs/data', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=hello`;
-
-    axios
-      .get(url, { headers })
-      .then((response) => {
-        console.log("resABC");
-        res.json(response.data.user);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    const response = await fetch(`https://www.instagram.com/${req.body.input}/?__a=1&__d=dis`);
+    const resp = await response.json();
+    console.log(resp);
+    res.json(resp);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch data' });
   }
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
