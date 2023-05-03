0e4462312f3b538d711b997199e5dcfc652ae8f8
basantimages <124242939+basantimages@users.noreply.github.com>
2023-05-04 01:34:28 +0530

    proxy added

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-
 import cors from 'cors';
 import express from 'express';
 import fetch from 'node-fetch';
+import ProxyAgent from 'proxy-agent';
+import { getProxies } from 'proxy-chain';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Function to get a random proxy from the list of proxies
+const getRandomProxy = async () => {
+  const proxies = await getProxies('http://proxy-chain-db.herokuapp.com/getProxies');
+  const randomProxy = proxies[Math.floor(Math.random() * proxies.length)];
+  return randomProxy;
+};
+
 app.post('/', async (req, res) => {
   try {
-    const response = await fetch(`https://www.instagram.com/${req.body.input}/?__a=1&__d=dis`);
+    const proxy = await getRandomProxy();
+    const agent = new ProxyAgent(proxy);
+    const response = await fetch(`https://www.instagram.com/${req.body.input}/?__a=1&__d=dis`, {
+      agent,
+    });
     const resp = await response.json();
     console.log(resp);
     res.json(resp);
@@ -20,7 +32,11 @@ app.post('/', async (req, res) => {
 
 app.get('/', async (req, res) => {
   try {
-    const response = await fetch(`http://www.instagram.com/hello/?__a=1&__d=dis`);
+    const proxy = await getRandomProxy();
+    const agent = new ProxyAgent(proxy);
+    const response = await fetch(`https://www.instagram.com/hello/?__a=1&__d=dis`, {
+      agent,
+    });
     const resp = await response.json();
     console.log(resp);
     res.json(resp);
