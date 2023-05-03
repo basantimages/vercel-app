06dd3fbc9602e0a9bdd1fd0f8077704f132aacbf
basantimages <124242939+basantimages@users.noreply.github.com>
2023-05-04 00:35:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post('/brs/data', async (req, res) => {
 
 app.get('/brs/data', async (req, res) => {
   try {
-    const response = await fetch(`https://www.instagram.com/${req.body.input}/?__a=1&__d=dis`);
+    const response = await fetch(`https://www.instagram.com/hello/?__a=1&__d=dis`);
     const resp = await response.json();
     console.log(resp);
     res.json(resp);
